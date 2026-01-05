7974b598c395d4e32b7b6fc4318142c81d591ca3
iMen <aminalfarisi.id@gmail.com>
2026-01-05 22:06:40 +0700

    DONE

---
+++ Learn Python/2.py
@@ -0,0 +1,25 @@
+# VARIABEL adalah tempat menyimpan data
+
+# menaru / assigment nilai
+
+a = 10
+x = 5
+panjang = 1000
+
+print("nilai a =", a)
+print("nilai x =", x)
+print("nilai panjang =", panjang)
+
+# penamaan
+nilai_y = 15 #dengan str 
+juta10 = 10000000 #tidak boleh angka di awal
+nilaiZ = 17.5 #nama unta
+
+# pemanggilan kedua
+print("Nilai a =", a)
+a = 7
+print("Nilai a =", a)
+
+#assigment indirect
+b = a
+print("Nilai b", b)
