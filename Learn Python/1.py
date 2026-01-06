be3478dfd2d10301c510dfbb0e2d7df5ef1a33f2
iMen <aminalfarisi.id@gmail.com>
2026-01-06 22:39:30 +0700

    Done

--- Learn Python/1.py
+++ Learn Python/1.py
@@ -1,1 +1,22 @@
 # How Programs and Bytecode Work
+import time
+star time = time.time()
+
+print ("Brudak HYTAM")
+print ("Sobat IRENG")
+print ("JAWA")
+
+#this Comment
+a = 10
+"""Samlekom mamank JAWIR
+yang sangat
+IIIIIRRREENGG disana"""
+
+print(a)
+for i in range(1,1000)
+    a = 10
+    
+print(time.time() - start_time, "detik")
+
+# we can compiling python to bytecode
+# python -m py_compile main.py
