e73462a50d297e84b0111f6928826c55547435e6
iMen <aminalfarisi.id@gmail.com>
2026-01-12 12:49:00 +0700

    1:35:00

---
+++ 5.py
@@ -0,0 +1,20 @@
+# OPERATOR
+
+# Operator Aritmatika
+a = 10
+b = 3
+print(a+b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(a // b)   # bilangan bulat (hasil tanpa desimal)
+print(a % b)    # Modulo (sisa pembagian)
+# praktis Modulo
+print(10 % 2)
+print(11 % 2)
+
+a = 2
+b = 3
+print(a ** b)
+print(5 ** 2)
