5fc7b457bd82b77550b942bc5db625f9839b9201
iMen <aminalfarisi.id@gmail.com>
2026-01-13 22:06:12 +0700

    1:45:00

--- Learn Python/5.py
+++ Learn Python/5.py
@@ -18,3 +18,40 @@ a = 2
 b = 3
 print(a ** b)
 print(5 ** 2)
+
+# 2\ Operator Assignment
+x = 10
+print("x awal:", x) #example
+print(x)
+
+x += 5
+print("x setelah +=5:", x) # 15
+print (x)
+
+x -= 3
+print(x)
+
+x /= 5
+print(x)
+
+x *= 2
+print(x)
+
+# 3\ Operator Perbandingan
+print(a > b)    # lebih besar: True
+print(a < b)    # lebih kecil: False
+print(a >= b)   # lebih besar atau sama: True
+print(a <= b)   # lebih kecil atau sama: False
+print(a == b)   # sama dengan: False
+print(a != b)   # tidak sama denga: True
+
+code1 = "coding"
+code2 = "vibeCoding"
+code3 = "Fusnuk"
+
+print(code1 == code3)
+print(code3 != code2)
+
+# 4\ Operator Logic
+pekerja = 19000000
+print(pekerja > 19000000 and pekerja < 100000)
