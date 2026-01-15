c35986fd5c69694e6b85454ca054bc11236e60f8
iMen <aminalfarisi.id@gmail.com>
2026-01-15 14:29:02 +0700

    2:00:00

---
+++ Learn Python/6.py
@@ -0,0 +1,18 @@
+# STRUCTURE CONTROL
+
+# 1\ If
+angka = int(input("masukan angka: "))
+if angka > 0:
+    print("angka positif")
+if angka < 0:
+    print("angka negatif")
+
+if angka == 0:
+    print("angka nol")
+
+# 2\ If-Else
+nilai = int(input("masukan nilai: "))
+if nilai >= 60:
+    print("Anda Lulus")
+else:
+    print("Anda tidak Lulus")
