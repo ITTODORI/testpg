39e65011b0c278b5c221195eb8d7b2546c106590
iMen <aminalfarisi.id@gmail.com>
2026-01-18 13:32:21 +0700

    2:35:00

--- Learn Python/7.py
+++ Learn Python/7.py
@@ -17,6 +17,40 @@ for huruf in framework:
 
 Database = input("Platform")
 for huruf in Database:
-    print("=", Database)
+    print("-", Database)
 
 # 3\ While Loop
+hutang = 1
+while hutang <= 5:
+    print(hutang)
+    hutang += 1         #fungsi penghentian loop
+
+jawaban = ""
+while jawaban != "Bahlil":
+    jawaban = input("Siapa mentri ESDM? ")
+    if jawaban != "Bahlil":
+        print("di bilangin \'Bahlil\' Tololll....!")
+print("Lu yang milih, bukan gw!")
+
+# 4\ Break and Continues
+ijazah_rahasia = 7
+
+while True:
+    jawaban = int(input("Presiden Prabowo berada di nomor berapa? "))
+
+    if jawaban == ijazah_rahasia:
+        print("1, 2, 3. OKE GAS!")
+        break
+    else:
+        print("tahun 2025")
+
+ijazah_rahasia = "Mulyono"
+
+while True:
+    jawaban = input("Siapa nama kecil/asli Presiden ke-7? ")
+    
+    if jawaban == ijazah_rahasia:
+        print("Waghh..! KAGET")
+        break
+    else:
+        print("whoo.. yaa ndak tau kok tanya saya")
