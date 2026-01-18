509b869b854e0e770c90f3dcd7433079080faa5d
iMen <aminalfarisi.id@gmail.com>
2026-01-18 09:10:13 +0700

    2:25:00

---
+++ Learn Python/7.py
@@ -0,0 +1,11 @@
+# LOOPING
+# 1\ for Loop
+for i in range(5):
+    print("Hedoeepp Jo Ko Wee !!")
+    print(i)
+
+for i in range(1,6):
+    print("Deforestasi Sawit", 1)
+
+for i in range(10, 0, -1):
+    print("Ijazahmu Palsu!", i)
