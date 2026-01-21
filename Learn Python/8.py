2e4fc8903cb57d20d22ba1bc8642d9f4814195cc
iMen <aminalfarisi.id@gmail.com>
2026-01-21 21:39:40 +0700

    2:50:00

--- Learn Python/8.py
+++ Learn Python/8.py
@@ -1,3 +1,10 @@
 # DATA STRUCTURE
 # 1\ List : variabel ini sangat dinamis bisa campuran, hanya huruf/angka.
 listEmpty  = []
+print(listEmpty)
+
+topHero = ["Termulator","Bahlol", "PraboGO", "luHUB"]
+print(topHero)
+
+statHero = ["Termulator", 90, "Bahlol", 60, "PraboGO", 85]
+print(statHero)
