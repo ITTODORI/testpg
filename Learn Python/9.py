4c2e6446f74fe6bc54a5f92d5ffdf4b457cc7d9d
iMen <aminalfarisi.id@gmail.com>
2026-01-24 11:32:08 +0700

    3:25:00

--- Learn Python/9.py
+++ Learn Python/9.py
@@ -8,3 +8,15 @@ govt_function()
 govt_function()
 
 # 2\ Parameter
+def pejabat(nama):
+    print("WOI babu", nama)
+    print("Lu kerjanya ngapain aja sih?!")
+
+pejabat("Bahlol")
+pejabat("luHUB")
+
+def info(lahan, sawit):
+    pekerja = lahan * sawit
+    print("Tenaga Kerja: ", pekerja, "Pekerja")
+
+info(250, 76000)
