2671e5093b15f81eed6d5088502fe51e7666cb92
iMen <aminalfarisi.id@gmail.com>
2026-01-24 11:19:19 +0700

    3:20:00

---
+++ Learn Python/9.py
@@ -0,0 +1,10 @@
+# FUNCTION
+# 1\ Example Function
+def govt_function():
+    print("Apa ada fungsinya pemerintah?!") #run
+    
+#run function    
+govt_function()
+govt_function()
+
+# 2\ Parameter
