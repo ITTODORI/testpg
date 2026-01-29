8ce52c83aedd6291e02365b53d7c438209271688
iMen <aminalfarisi.id@gmail.com>
2026-01-29 06:54:01 +0000

    3:06:00

--- Learn Python/11.py
+++ Learn Python/11.py
@@ -1,28 +1,28 @@
 # Fixed Error
 # 1\ Try-Except
-# print("=== DATA CORRUPTION GOVT ===")
+print("=== DATA CORRUPTION GOVT ===")
 
-# try:
-#     buget = int(input("Buget: "))
-#     expend = int(input("Expend: "))
-#     hasil = buget / expend
-#     print("Corrupt:", hasil)
-# except:
-#     print("Terjadi error dalam perhitungan bahlil!")
-# print("=== MENUJU GENERASI CEMAS ===")
+try:
+    buget = int(input("Buget: "))
+    expend = int(input("Expend: "))
+    hasil = buget / expend
+    print("Corrupt:", hasil)
+except:
+    print("Terjadi error dalam perhitungan bahlil!")
+print("=== MENUJU GENERASI CEMAS ===")
 
 # 2\ Specifik Error
-# try:
-#     x = int(input("Horizontal: "))
-#     y = int(input("Vertical: "))
-#     size = x / y
-#     print("Size ration:", size)
-# except ValueError:
-#     print("Masukan value")
-# except ZeroDivisionError:
-#     print("Ratio tidak cocok!")
-# except:
-#     print("Terjadi Error")
+try:
+    x = int(input("Horizontal: "))
+    y = int(input("Vertical: "))
+    size = x / y
+    print("Size ration:", size)
+except ValueError:
+    print("Masukan value")
+except ZeroDivisionError:
+    print("Ratio tidak cocok!")
+except:
+    print("Terjadi Error")
 
 # 3\ Try-Execpt-Else
 try:
