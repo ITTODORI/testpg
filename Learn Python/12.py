ba2028b6852f0566abbc889d944021ad06701e58
iMen <aminalfarisi.id@gmail.com>
2026-01-30 03:22:46 +0000

    4:15:00

--- Learn Python/12.py
+++ Learn Python/12.py
@@ -1,2 +1,23 @@
 # FILE INPUT & OUTPUT
 # 1\ file I/O (Input/Output)
+
+# Mode File
+# r # to Read
+# w # to Write
+# a # to Append
+# x # to Creat
+# write(string)
+print("=== TITLE DATABASE ===")
+file = open("12-DATABASE.txt", "w")
+
+while True:
+    hero = input("Hero: ")
+    if hero == "":
+        break
+    stats = input("Stats: ")
+
+    file.write(hero + ", " + stats + "\n")
+    print("Database", hero, "has saved")
+
+file.close()
+print("=== Program is DONE! ===")
