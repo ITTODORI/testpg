6dab949670c33112e5892bf794fab0d2f1093e0e
iMen <aminalfarisi.id@gmail.com>
2026-02-05 15:25:17 +0000

    Standar Library

--- Learn Python/Basic/15.py
+++ Learn Python/Basic/15.py
@@ -3,4 +3,5 @@ import datetime
 
 data_time = datetime.datetime.now()
 print(data_time)
-print(f"Time Global > {data_time}")
+print(f"Year > {data_time.year}") #in Library datetime
+print(f"Day > {data_time.strftime('%A')}") #in Library datetime
