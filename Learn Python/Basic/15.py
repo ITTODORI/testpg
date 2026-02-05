d7255b3b6f06134b0c0d8e33c68692e6a86465db
iMen <aminalfarisi.id@gmail.com>
2026-02-05 15:32:22 +0000

    Stanadr Library

--- Learn Python/Basic/15.py
+++ Learn Python/Basic/15.py
@@ -5,3 +5,19 @@ data_time = datetime.datetime.now()
 print(data_time)
 print(f"Year > {data_time.year}") #in Library datetime
 print(f"Day > {data_time.strftime('%A')}") #in Library datetime
+
+# Collection
+from collections import Counter
+
+count = 0
+data = ["a", "b", "c", "d"]
+for i in data:
+    if i == "a":
+        count += 1
+print(count)
+
+# - Simply
+data = ["a", "b", "c", "d"]
+data_count = Counter(data)
+
+print(f"Data Count = {data_count}")
