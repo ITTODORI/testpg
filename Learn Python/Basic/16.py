04f16ac6baac7c5e4f00448da0bd9c2d0c895845
iMen <aminalfarisi.id@gmail.com>
2026-02-08 14:45:13 +0000

    GUI

---
+++ Learn Python/Basic/16.py
@@ -0,0 +1,13 @@
+# GUI : Graphical User Interface
+import tkinter as tk
+
+app = tk.Tk()
+app.configure(bg="black")
+app.geometry("1080x1920")
+app.resizable(False,False)
+app.title("Mini Apps")
+
+# Frame Input
+input_frame = 
+
+app.mainloop()
