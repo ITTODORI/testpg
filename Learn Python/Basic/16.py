bb9a0a278819c9c3e963faff41fb2de865ba2e24
iMen <aminalfarisi.id@gmail.com>
2026-02-08 15:07:24 +0000

    GUI

--- Learn Python/Basic/16.py
+++ Learn Python/Basic/16.py
@@ -9,13 +9,14 @@ app.title("Mini Apps")
 
 EMAIL = ttk.StringVar()
 PASSWORD = ttk.StringVar()
+
 # Frame Input
 input_frame = ttk.Frame(app)
 # Place, Grid, Pack
 input_frame.pack(padx=10,pady-10,fill="x",expand=True)
 
 # Components
-# Email
+# - Email
 mail_label = ttk.Label(input_frame, text="Email")
 mail_label.pack(padx=10,fill="x",expand=True)
 
@@ -23,12 +24,18 @@ mail_label.pack(padx=10,fill="x",expand=True)
 mail_entry = ttk.Entry(input_frame, textvariable=EMAIL)
 mail_entry.pack(padx=10,fill="x",expand=True)
 
-# Password
+# - Password
 pass_label = ttk.Label(input_frame, text="Email")
 pass_label.pack(padx=10,fill="x",expand=True)
 
 pass_entry = ttk.Entry(input_frame, textvariable=PASSWORD)
 pass_entry.pack(padx=10,fill="x",expand=True)
 
+# Button
+def button_click():
+    print(f"Login Succes {EMAIL}.get()")
+
+click_button = ttk.Button(input_frame,text="Login",command=button_click)
+click_button.pack(fill='x', expand=True,pady=10,padx=10)
 
 app.mainloop()
