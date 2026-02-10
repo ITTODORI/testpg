79ee588026adb67d6574416cca6d393d0413e5fe
iMen <aminalfarisi.id@gmail.com>
2026-02-10 14:13:11 +0000

    PIP

--- Learn Python/Basic/17.py
+++ Learn Python/Basic/17.py
@@ -1,1 +1,6 @@
-# PIP
+# PACKAGE INDEX PYTHON 
+# pypi.org /Resources package index pthon
+# pip --version
+# pip list
+# python -m pip install --upgrade pip /install upgrade pip version
+# pip uninstall <pip> / delete, remove, uninstal
