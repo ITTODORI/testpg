f715bf3069bd1b59d78aec49ba109118b9112595
iMen <aminalfarisi.id@gmail.com>
2026-02-10 14:30:13 +0000

    PIP

--- Learn Python/Basic/17.py
+++ Learn Python/Basic/17.py
@@ -12,7 +12,7 @@ list_a = [1,2,3,4]
 vector_a = np.array([1,2,3,4])
 
 print(f"list a = {list_a}")
-print(list_a**2) # error
+#print(list_a**2) # error
 print(f"vector a = {vector_a}")
 print(f"a pangkat > {vector_a**2}")
 print(f"a x 5 > {vector_a*5}")
@@ -28,4 +28,4 @@ ones_d = np.ones((2,2))
 print(f"Ones : \n{ones_d}")
 
 sum_bbd = matrix_b + matrix_b**2 + ones_d
-print(f"Sum = \n{sum_bbd}")
+print(f"Sum : \n{sum_bbd}")
