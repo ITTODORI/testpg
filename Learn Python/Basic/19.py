03d2ea26380d796febe28877a57255b6ef646067
iMen <aminalfarisi.id@gmail.com>
2026-02-14 15:23:09 +0000

    Programe Gate

--- Learn Python/Basic/19.py
+++ Learn Python/Basic/19.py
@@ -3,4 +3,13 @@
 
 print(f"value ___name___ 19.py = '{__name__}'")
 
-import fungsi
+import function 
+
+def function_main(a:int, b:int) -> int:
+    return a+b
+
+if __name__ == "__main__":
+    v1 = 5
+    v2 = 10
+    result = function_main(v1, v2)
+    print(f"Result Program Gate = {result}")
